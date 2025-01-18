66e439c377080ce27dd9ce87368e0f8dc35944f4
ninad-2022 <ninad@quolam.com>
2025-01-18 17:15:31 +0530

    added onde

--- typeScript/l5.1-one.js
+++ typeScript/l5.1-one.js
@@ -1,15 +1,15 @@
 "use strict";
-exports.__esModule = true;
-exports.cityList = exports.add = exports.city = void 0;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.cityList = exports.city = void 0;
+exports.add = add;
 var city = "pune";
 exports.city = city;
 function add(a, b) {
     return a + b;
 }
-exports.add = add;
 var cities = ["Pune", "Mumbai", "Nashik"];
 exports.cityList = cities;
 function square(a) {
     a * a;
 }
-exports["default"] = square;
+exports.default = square;
