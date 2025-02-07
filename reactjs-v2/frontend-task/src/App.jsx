2baa4a201c8ef49dcb9174e496447f2f96972202
ninad-2022 <ninad@quolam.com>
2025-02-07 22:08:24 +0530

    task: search input

---
+++ reactjs-v2/frontend-task/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import AutoSearchInput from "./components/AutoSearchInput/Index";
+
+function App() {
+  return (
+    <>
+      <AutoSearchInput />
+    </>
+  );
+}
+
+export default App;
---
+++ reactjs-v2/frontend-task/src/components/AutoSearchInput/Index.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import "./Index.css";
+
+const AutoSearchInput = () => {
+  const [search, setSearch] = useState("");
+  const [foodItems, setFoodItems] = useState([]);
+  const [showContent, setShowContent] = useState(false);
+  const [cache, setCache] = useState({});
+
+  const handleSearchChange = (e) => {
+    console.log(e.target.value);
+    setSearch(e.target.value);
+  };
+
+  const fetchFoodItems = async () => {
+    if (cache[search]) {
+      setFoodItems(cache?.recipes);
+    }
+    const fetchFoodItems = await fetch(
+      `https://dummyjson.com/recipes/search?q=${search}`
+    );
+    const food = await fetchFoodItems.json();
+    setFoodItems(food?.recipes);
+    setCache((prev) => ({
+      ...prev,
+      [search]: food?.recipes,
+    }));
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(fetchFoodItems, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [search]);
+  return (
+    <>
+      <h2>Auto Search</h2>
+      <div className="parent-search">
+        <input
+          value={search}
+          name="searchBar"
+          id="searchBar"
+          onChange={handleSearchChange}
+          onFocus={() => setShowContent(true)}
+          onBlur={() => setShowContent(false)}
+        />
+        {showContent ? (
+          <div className="search-content">
+            {foodItems?.map((item) => {
+              return <p key={item.id}>{item.name}</p>;
+            })}
+          </div>
+        ) : null}
+      </div>
+    </>
+  );
+};
+export default AutoSearchInput;
